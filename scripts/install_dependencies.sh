896e16819869d7d85746490214bbb614c39ad5b2
Paul Barbenheim <paul@barbenheim.de>
2026-02-02 16:53:01 +0100

    add scripts

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+
+cd ../packages/jlcrypto
+echo "####### Installing jlcrypto dependencies #######"
+
+flutter pub get --enforce-lockfile
+
+echo "############## Finished jlcrypto ###############"
+echo ""
+echo ""
+
+
+cd ../jldb
+
+echo "######### Installing jldb dependencies #########"
+
+flutter pub get --enforce-lockfile
+
+echo "######## Generating jldb intermediates #########"
+
+flutter pub run build_runner build --delete-conflicting-outputs
+
+echo "################ Finished jldb #################"
+echo ""
+echo ""
+echo ""
+
+
+cd ../..
+
+echo "######### Installing app dependencies ##########"
+
+flutter pub get --enforce-lockfile
+
+echo "######### Generating app intermediates #########"
+
+flutter pub run build_runner build --delete-conflicting-outputs
+
+echo "################# Finished app #################"
