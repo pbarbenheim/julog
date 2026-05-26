ca8c848c8f371f825a96b7a6921879a7c04c01e8
Paul Barbenheim <paul@barbenheim.de>
2026-05-26 20:32:05 +0200

    Fix lockfile enforcing

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 cd ../packages/jlcrypto
 echo "####### Installing jlcrypto dependencies #######"
 
-dart pub get --enforce-lockfile
+dart pub get
 
 echo "############## Finished jlcrypto ###############"
 echo ""
@@ -17,7 +17,7 @@ cd ../jldb
 
 echo "######### Installing jldb dependencies #########"
 
-dart pub get --enforce-lockfile
+dart pub get
 
 echo "######## Generating jldb intermediates #########"
 
